0c4a27b4285e441ae89325de5466e9056885f03a
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:30:35 +0300

    syntax fixes

--- src/components/DefinitionComponent.tsx
+++ src/components/DefinitionComponent.tsx
@@ -1,36 +1,39 @@
-import { Card, CardContent, Typography, Box } from '@mui/material';
-import Definition from './../models/Definition';
+import { Card, CardContent, Typography, Box } from "@mui/material";
+import Definition from "./../models/Definition";
 
 interface PropTypes {
-    data: Definition
+  data: Definition;
 }
 
 const DefinitionComponent = (props: PropTypes) => {
-    return (
-        <Card sx={{ minWidth: 275, maxWidth: 410, mx: "auto", my: 3 }}>
-            <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    Definition
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {props.data.word}
-                </Typography>
-                <Typography variant="body2">
-                    {props.data.definition}
-                </Typography>
-                {props.data.example.length > 0
-                    ? <Box>
-                        <Typography variant="body2" sx={{ fontSize: 14, pt: 1 }} color="text.secondary" gutterBottom>
-                            Example
-                        </Typography>
-                        <Typography variant="body2">
-                            {props.data.example}
-                        </Typography>
-                    </Box>
-                    : ""}
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card sx={{ minWidth: 275, maxWidth: 410, mx: "auto", my: 3 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Definition
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props.data.word}
+        </Typography>
+        <Typography variant="body2">{props.data.definition}</Typography>
+        {props.data.example.length > 0 ? (
+          <Box>
+            <Typography
+              variant="body2"
+              sx={{ fontSize: 14, pt: 1 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Example
+            </Typography>
+            <Typography variant="body2">{props.data.example}</Typography>
+          </Box>
+        ) : (
+          ""
+        )}
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DefinitionComponent;
